b6fe5058f80c3311484c0092d4c31eafa9b2cdcb
essmoncif <moncefessaoudi@gmail.com>
2021-03-09 18:20:37 +0100

    storage module

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { StorageService } from './storage.service';
+import { StorageController } from './storage.controller';
 
-@Module({})
+@Module({
+  providers: [StorageService],
+  controllers: [StorageController]
+})
 export class StorageModule {}
