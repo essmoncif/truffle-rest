9c14fc752ec483b43a9699fbb9467ed26e85a4d5
essmoncif <moncefessaoudi@gmail.com>
2021-03-10 17:45:55 +0100

    fix guard

--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -1,28 +1,55 @@
-import { Body, Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from 'src/shared/auth.guard';
+import { Body, Controller, Get, Post,UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '../shared/auth.guard';
 import { Account } from './storage.decorator';
 import { LoginDTO, SetDataDTO } from './storage.dto';
 import { StorageService } from './storage.service';
 
+@ApiTags('Storage')
 @Controller('storage')
 export class StorageController {
 
     constructor(private readonly storageService: StorageService){}
 
+
+
     @Post('/login')
+    @ApiCreatedResponse({ description: 'The token has been successfully created.'})
+    @ApiResponse({ status: 400, description: 'invalid address account !'})
     async login(@Body() loginDTO: LoginDTO){
         return this.storageService.login(loginDTO)
     }
 
-    @UseGuards(new AuthGuard())
+
+
+
+    
     @Post()
-    async setValue(@Body() setDataDTO: SetDataDTO, @Account("account") account: string){
+    @ApiBody({
+        description: "Set new value ",
+        type: SetDataDTO,
+    })
+    @ApiBearerAuth()
+    @ApiCreatedResponse({ description: 'The value has been successfully updated.'})
+    @ApiResponse({ status: 400, description: 'invalid value !'})
+    @UseGuards(new AuthGuard())
+    async setValue(@Body() setDataDTO: SetDataDTO, @Account("account") account){
+        console.log(account)
         return (this.storageService.setData(setDataDTO, account));
     }
 
-    @UseGuards(new AuthGuard())
+
+
+
+
+    
     @Get()
-    async getValue(@Account("account") account: string){
+    @ApiBearerAuth()
+    @ApiOkResponse({ description: 'value inside a smart contract.'})
+    @ApiResponse({ status: 400, description: 'invalid address account !'})
+    @UseGuards(new AuthGuard())
+    async getValue(@Account("account") account){
+        console.log(account)
         return (this.storageService.getData(account));
     }
 }
