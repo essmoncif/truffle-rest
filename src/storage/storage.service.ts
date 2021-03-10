9f464ae3e815ce9f7ca48b939571963e5bf7d89b
essmoncif <moncefessaoudi@gmail.com>
2021-03-10 12:55:20 +0100

    add login meth

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import web3 from '../shared/web3.provider';
-import { SetDataDTO } from './storage.dto';
+import { LoginDTO, SetDataDTO } from './storage.dto';
+import * as jwt from "jsonwebtoken";
 import * as SimpleStorageArtifact from '../../build/contracts/SimpleStorage.json';
 const TruffleContract = require('@truffle/contract')
 
@@ -15,6 +16,15 @@ export class StorageService {
 
     constructor(){
         this.web3Provider = web3;
+    }
+
+    async login(loginDTO: LoginDTO){
+    
+        const { account} = loginDTO;
+        if(web3.utils.isAddress(account))
+            return {"token" : await jwt.sign({account}, process.env.JWT_SECRET, { expiresIn: '7d' })} 
+        else
+            throw new HttpException('Invalid address', HttpStatus.BAD_REQUEST);
     }
 
     async setData(data: SetDataDTO, account){
