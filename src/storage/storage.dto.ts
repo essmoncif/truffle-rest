d9fb10196f1227008ad8f35eeaf6f1715499ed58
essmoncif <moncefessaoudi@gmail.com>
2021-03-10 17:53:28 +0100

    add ApiProperty to DTO

--- src/storage/storage.dto.ts
+++ src/storage/storage.dto.ts
@@ -1,14 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, IsNotEmpty, IsString, Min } from "class-validator";
 
 export class SetDataDTO {
     @IsNotEmpty()
     @IsInt()
     @Min(0)
+    @ApiProperty({
+        description: "Value",
+        type: Number
+    })
     value : number;
 }
 
 export class LoginDTO {
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({
+        description: "Accout address",
+        type: String
+    })
     account : string;
 }
