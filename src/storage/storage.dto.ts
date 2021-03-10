0c1e50f806070da027f6830a1837736a7576436e
essmoncif <moncefessaoudi@gmail.com>
2021-03-10 12:56:30 +0100

    add login dto

--- src/storage/storage.dto.ts
+++ src/storage/storage.dto.ts
@@ -1,8 +1,14 @@
-import { IsInt, IsNotEmpty, Min } from "class-validator";
+import { IsInt, IsNotEmpty, IsString, Min } from "class-validator";
 
 export class SetDataDTO {
     @IsNotEmpty()
     @IsInt()
     @Min(0)
     value : number;
+}
+
+export class LoginDTO {
+    @IsNotEmpty()
+    @IsString()
+    account : string;
 }
