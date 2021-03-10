5498dd4f4bd20d625cdf64e388e6633638a7896b
essmoncif <moncefessaoudi@gmail.com>
2021-03-10 12:55:55 +0100

    add login endpoint

--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/shared/auth.guard';
 import { Account } from './storage.decorator';
-import { SetDataDTO } from './storage.dto';
+import { LoginDTO, SetDataDTO } from './storage.dto';
 import { StorageService } from './storage.service';
 
 @Controller('storage')
@@ -8,11 +9,18 @@ export class StorageController {
 
     constructor(private readonly storageService: StorageService){}
 
+    @Post('/login')
+    async login(@Body() loginDTO: LoginDTO){
+        return this.storageService.login(loginDTO)
+    }
+
+    @UseGuards(new AuthGuard())
     @Post()
     async setValue(@Body() setDataDTO: SetDataDTO, @Account("account") account: string){
         return (this.storageService.setData(setDataDTO, account));
     }
 
+    @UseGuards(new AuthGuard())
     @Get()
     async getValue(@Account("account") account: string){
         return (this.storageService.getData(account));
